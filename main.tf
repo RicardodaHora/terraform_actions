534bca7ea906a82775df773212134ec0ba3c29c9
ricardo.hora <ricardo.hora@mjv.com.br>
2023-12-15 16:18:44 -0300

    Terraform git actions

---
+++ main.tf
@@ -0,0 +1,27 @@
+ provider "google-beta"{
+   project = var.project
+   region = var.region
+ }
+
+ data "google_secret_manager_secret" "existing_secret" {
+   provider = google-beta
+   secret_id = "dataform"
+ }
+
+ data "google_secret_manager_secret_version" "existing_secret_version" {
+   provider = google-beta
+   secret = data.google_secret_manager_secret.existing_secret.id
+   version = "latest"
+ }
+
+resource "google_dataform_repository" "dataform_respository" {
+  provider = google-beta
+  name = "dataform"
+
+  git_remote_settings {
+      url = var.remote_url
+      default_branch = "dataform-test"
+      authentication_token_secret_version = data.google_secret_manager_secret_version.existing_secret_version.id      
+  }
+
+ }
---
+++ variables.tf
@@ -0,0 +1,53 @@
+
+variable "project" {
+  type    = string
+  default = "terraform-406817"
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
+
+variable "service_account" {
+  type    = string
+  default = "service-491703034110@gcp-sa-dataform.iam.gserviceaccount.com"
+}
+
+variable "sa2" {
+  type    = string
+  default = "terraform@terraform-406817.iam.gserviceaccount.com"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "secret_git" {
+  type    = string
+  default = "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"
+}
+
+variable "secret_data" {
+  type    = string
+  default = "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"
+}
+
+#variable "credentials_file" { }
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+#variable "remote_url" { }
+
+
+
+
+
+
+
+
+
+
