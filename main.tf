f28004cba7bbb5571c2f5e135e05eb5941d52745
ricardo.hora <ricardo.hora@mjv.com.br>
2023-12-18 14:33:27 -0300

    Teste final 6

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.5.0"
+    }
+  }
+}
+
  provider "google-beta"{
    project = var.project
    region = var.region
