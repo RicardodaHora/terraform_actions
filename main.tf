e4a6408e147ac6d638485dd6c6e760188e375dbb
ricardo.hora <ricardo.hora@mjv.com.br>
2024-02-06 08:39:06 -0300

    criar tabela 4

--- main.tf
+++ main.tf
@@ -1,15 +1,23 @@
-variable "dataset_id" {
-  description = "O ID do conjunto de dados do BigQuery"
-  type        = string
-  default     = "terraform-406817"
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.5.0"
+    }
+  }
 }
+
+ provider "google-beta"{
+   project = var.project
+   region = var.region
+ }
 
 resource "google_bigquery_dataset" "default" {
   dataset_id = var.dataset_id
   location   = "US"
 }
 
-resource "google_bigquery_table" "default" {
+resource "google_bigquery_table" "tables" {
   dataset_id = google_bigquery_dataset.default.dataset_id
   table_id   = "tabela_test"
 
