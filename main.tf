5c40d1793c3c29311e2b1443aa53bae939f83cfc
ricardo.hora <ricardo.hora@mjv.com.br>
2024-02-06 14:02:57 -0300

    varias tabelas 2

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("terraform-406817-71a350626408.json")
+   project = "terraform-406817"
+   region = "US"
+}
+variable "datasets"
+locals{
+    datasets = var.datasets
+    tables = flatten([
+        for dataset_i, tables in local.var.datasets:[
+            for table in tables:{
+                dataset_id = dataset_id
+                table_id = table.table_id
+                schema_file = table.schema_file
+            }
+        ]
+    ])
+}
+
+resource "google_bigquery_table" "tables" {
+    for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
+    dataset_id = each.value.dataset_id
+    table_id =   each.value.table_id
+    schema = file(each.value.schema_file)
+    depends_on = [ 
+        google_bigquery_dataset.datasets
+     ]
+}
+
+resource "google_bigquery_dataset" "datasets" {
+    for_each = local.datasets
+    dataset_id = each.key
+}
