eaa36fd1cbbbd11b9d6523ca169f3c395a84d612
ricardo.hora <ricardo.hora@mjv.com.br>
2024-02-15 17:02:00 -0300

    Incluindo nova tabela 7

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "google_bigquery_table" "tables" {
 
   provisioner "local-exec" {
     command = <<EOF
-      if ! bq --project_id=${google.project} ls ${each.value.dataset_id}:${each.value.table_id} > /dev/null 2>&1; then
+      if ! bq --project_id=${project} ls ${each.value.dataset_id}:${each.value.table_id} > /dev/null 2>&1; then
         exit 1
       fi
     EOF
