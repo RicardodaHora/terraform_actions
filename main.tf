1f590bf2df1f957f0e66687ee31961e90f69e84e
ricardo.hora <ricardo.hora@mjv.com.br>
2024-02-15 17:23:45 -0300

    Incluindo nova tabela 12

--- main.tf
+++ main.tf
@@ -1,17 +1,34 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">=4.0" # Latest stable version
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.5.0"
     }
   }
 }
 
 provider "google" {
   credentials = file("terraform-406817-71a350626408.json")
-  project = "terraform-406817"
-  region = "US"
+   project = "terraform-406817"
+   region = "US"
 }
+variable "datasets" {
+
+}
+
+locals {
+  datasets = var.datasets
+  tables = flatten([
+    for dataset_id, tables in local.datasets:[
+      for table in tables : {
+        dataset_id = dataset_id
+        table_id = table.table_id
+        schema_file = table.schema_file
+      }
+    ]
+  ])
+}
+
 
 resource "google_bigquery_table" "tables" {
   for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
@@ -21,15 +38,47 @@ resource "google_bigquery_table" "tables" {
 
   provisioner "local-exec" {
     command = <<EOF
-      if ! bq --project_id=${google.project} ls ${each.value.dataset_id}:${each.value.table_id} > /dev/null 2>&1; then
+      if ! bq --project_id=${google.project.project_id} ls ${each.value.dataset_id}:${each.value.table_id} > /dev/null 2>&1; then
         exit 1
       fi
     EOF
     interpreter = ["/bin/sh", "-c"]
-    on_failure = continue
+    on_failure = "continue"
   }
 
   lifecycle {
     create_before_destroy = true
   }
 }
+
+
+
+# resource "google_bigquery_table" "tables" {
+#     for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
+#     dataset_id = each.value.dataset_id
+#     table_id =   each.value.table_id
+#     schema = file(each.value.schema_file)
+
+#     # Verifica se a tabela já existe
+#     depends_on = [google_bigquery_table_exists.check_table]
+
+#     lifecycle {
+#       create_before_destroy = true
+#     }
+# }
+
+# resource "google_bigquery_table_exists" "check_table" {
+#   for_each = each.value
+
+#   project = google.project
+#   dataset_id = each.key.dataset_id
+#   table_id = each.key.table_id
+# }
+
+
+# resource "google_bigquery_dataset" "datasets" {
+#     for_each = local.datasets
+#     dataset_id = each.key
+# }
+
+
