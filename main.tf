05fc1cf4a96e5c2c3abe5b101168958cb4c751c2
ricardo.hora <ricardo.hora@mjv.com.br>
2024-02-15 17:46:38 -0300

    Incluindo nova tabela 18

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ resource "google_project" "project" {
   provider = google
   project_id = "terraform-406817"
   name = "Terraform"
-  parent = "organizations/12345678"
 }
 
 variable "datasets" {
@@ -38,48 +37,49 @@ locals {
   ])
 }
 
+# resource "google_bigquery_table" "tables" {
+#   for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
+#   dataset_id = each.value.dataset_id
+#   table_id =   each.value.table_id
+#   schema = file(each.value.schema_file)
+
+#   provisioner "local-exec" {
+#     command = <<EOF
+#       if ! bq --project_id=${google_project.project.project_id} ls ${each.value.dataset_id}:${each.value.table_id} > /dev/null 2>&1; then
+#         exit 1
+#       fi
+#     EOF
+#     interpreter = ["/bin/sh", "-c"]
+#     on_failure = continue
+#   }
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+
 resource "google_bigquery_table" "tables" {
-  for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
-  dataset_id = each.value.dataset_id
-  table_id =   each.value.table_id
-  schema = file(each.value.schema_file)
+    for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
+    dataset_id = each.value.dataset_id
+    table_id =   each.value.table_id
+    schema = file(each.value.schema_file)
 
-  provisioner "local-exec" {
-    command = <<EOF
-      if ! bq --project_id=${google_project.project.project_id} ls ${each.value.dataset_id}:${each.value.table_id} > /dev/null 2>&1; then
-        exit 1
-      fi
-    EOF
-    interpreter = ["/bin/sh", "-c"]
-    on_failure = continue
-  }
+    # Verifica se a tabela já existe
+    depends_on = [google_bigquery_table_exists.check_table]
 
-  lifecycle {
-    create_before_destroy = true
-  }
+    lifecycle {
+      create_before_destroy = true
+    }
 }
 
-# resource "google_bigquery_table" "tables" {
-#     for_each = {for table in local.tables : "${table.dataset_id}.${table.table_id}" => table}
-#     dataset_id = each.value.dataset_id
-#     table_id =   each.value.table_id
-#     schema = file(each.value.schema_file)
+resource "google_bigquery_table_exists" "check_table" {
+  for_each = each.value
 
-#     # Verifica se a tabela já existe
-#     depends_on = [google_bigquery_table_exists.check_table]
-
-#     lifecycle {
-#       create_before_destroy = true
-#     }
-# }
-
-# resource "google_bigquery_table_exists" "check_table" {
-#   for_each = each.value
-
-#   project = google.project
-#   dataset_id = each.key.dataset_id
-#   table_id = each.key.table_id
-# }
+  project = google_project.project.project_id
+  dataset_id = each.key.dataset_id
+  table_id = each.key.table_id
+}
 
 
 # resource "google_bigquery_dataset" "datasets" {
